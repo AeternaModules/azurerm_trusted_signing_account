e644c76f16be2bbd6953a87eba7dc82b4d3de7c7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "trusted_signing_accounts" {
-  description = "All trusted_signing_account resources"
-  value       = azurerm_trusted_signing_account.trusted_signing_accounts
-}
 output "trusted_signing_accounts_account_uri" {
-  description = "List of account_uri values across all trusted_signing_accounts"
-  value       = [for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : v.account_uri]
+  description = "Map of account_uri values across all trusted_signing_accounts, keyed the same as var.trusted_signing_accounts"
+  value       = { for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : k => v.account_uri }
 }
 output "trusted_signing_accounts_location" {
-  description = "List of location values across all trusted_signing_accounts"
-  value       = [for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : v.location]
+  description = "Map of location values across all trusted_signing_accounts, keyed the same as var.trusted_signing_accounts"
+  value       = { for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : k => v.location }
 }
 output "trusted_signing_accounts_name" {
-  description = "List of name values across all trusted_signing_accounts"
-  value       = [for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : v.name]
+  description = "Map of name values across all trusted_signing_accounts, keyed the same as var.trusted_signing_accounts"
+  value       = { for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : k => v.name }
 }
 output "trusted_signing_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all trusted_signing_accounts"
-  value       = [for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all trusted_signing_accounts, keyed the same as var.trusted_signing_accounts"
+  value       = { for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : k => v.resource_group_name }
 }
 output "trusted_signing_accounts_sku_name" {
-  description = "List of sku_name values across all trusted_signing_accounts"
-  value       = [for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : v.sku_name]
+  description = "Map of sku_name values across all trusted_signing_accounts, keyed the same as var.trusted_signing_accounts"
+  value       = { for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : k => v.sku_name }
 }
 output "trusted_signing_accounts_tags" {
-  description = "List of tags values across all trusted_signing_accounts"
-  value       = [for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : v.tags]
+  description = "Map of tags values across all trusted_signing_accounts, keyed the same as var.trusted_signing_accounts"
+  value       = { for k, v in azurerm_trusted_signing_account.trusted_signing_accounts : k => v.tags }
 }
 
